474aa5d89b2c21898c36e599ed9b8667499fbc9f
zhengran <zhengran@kuaishou.com>
2025-03-08 11:38:42 +0000

    use turn server

--- conn/ice_servers.go
+++ conn/ice_servers.go
@@ -3,14 +3,10 @@ package conn
 import "github.com/pion/webrtc/v4"
 
 var iceServers = []webrtc.ICEServer{
-	{URLs: []string{"stun:stun.l.google.com:19302"}},
-	{URLs: []string{"stun:stun.l.google.com:5349"}},
-	{URLs: []string{"stun:stun1.l.google.com:3478"}},
-	{URLs: []string{"stun:stun1.l.google.com:5349"}},
-	{URLs: []string{"stun:stun2.l.google.com:19302"}},
-	{URLs: []string{"stun:stun2.l.google.com:5349"}},
-	{URLs: []string{"stun:stun3.l.google.com:3478"}},
-	{URLs: []string{"stun:stun3.l.google.com:5349"}},
-	{URLs: []string{"stun:stun4.l.google.com:19302"}},
-	{URLs: []string{"stun:stun4.l.google.com:5349"}},
+	{
+		URLs:           []string{"turn:101.34.238.168:3478"},
+		Username:       "ranber",
+		Credential:     "12138",
+		CredentialType: webrtc.ICECredentialTypePassword,
+	},
 }
